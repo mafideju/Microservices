fc56ba102416aa8137b620713d35771f83d706d2
Marcio Rodrigues <mafideju@outlook.com>
2022-03-10 16:57:33 -0300

    feat(eureka): routing server

--- hr-payroll/src/main/java/com/mafideju/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mafideju/hrpayroll/services/PaymentService.java
@@ -1,13 +1,26 @@
 package com.mafideju.hrpayroll.services;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mafideju.hrpayroll.entities.Payment;
+import com.mafideju.hrpayroll.entities.Worker;
+import com.mafideju.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
+	
+//	@Value("${hr-worker.host}")
+//	private String workerHost;
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Nelio", 299.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	};
 }
