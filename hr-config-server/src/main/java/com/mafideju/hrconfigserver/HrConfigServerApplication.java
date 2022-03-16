9d0a1ee37c4ab02bb98ef3919f73720a4582f8fb
Marcio Rodrigues <mafideju@outlook.com>
2022-03-16 11:00:39 -0300

    feat(user): configuring hr user basics

---
+++ hr-config-server/src/main/java/com/mafideju/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,31 @@
+package com.mafideju.hrconfigserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication implements CommandLineRunner {
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+	
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("USERNAME " + username);
+		System.out.println("APP -> " + appName);
+		
+	}
+
+}
--- hr-worker/src/main/java/com/mafideju/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/mafideju/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mafideju.hrworker.entities.Worker;
 import com.mafideju.hrworker.repository.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value="/workers")
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -38,16 +44,23 @@ public class WorkerResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 		logger.info("***PORT*** = " + env.getProperty("local.server.port"));
 		
 		Worker worker = repository.findById(id).get();
 		
 		return ResponseEntity.ok(worker);
+	}
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("***CONFIG*** -> " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
